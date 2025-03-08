0f1936a2f70aa7cfd2d905f07ef13c234f85c2dd
VIVEK E M <vivekemplma@gmail.com>
2025-03-08 11:42:29 +0300

    Add files via upload

---
+++ Create_folder.bat
@@ -0,0 +1,33 @@
+md 	2-Jan-25
+md 	3-Jan-25
+md 	4-Jan-25
+md 	5-Jan-25
+md 	6-Jan-25
+md 	7-Jan-25
+md 	8-Jan-25
+md 	9-Jan-25
+md 	10-Jan-25
+md 	11-Jan-25
+md 	12-Jan-25
+md 	13-Jan-25
+md 	14-Jan-25
+md 	15-Jan-25
+md 	16-Jan-25
+md 	17-Jan-25
+md 	18-Jan-25
+md 	19-Jan-25
+md 	20-Jan-25
+md 	21-Jan-25
+md 	22-Jan-25
+md 	23-Jan-25
+md 	24-Jan-25
+md 	25-Jan-25
+md 	26-Jan-25
+md 	27-Jan-25
+md 	28-Jan-25
+md 	29-Jan-25
+md 	30-Jan-25
+md 	31-Jan-25
+
+
+bat file for folder creation.
